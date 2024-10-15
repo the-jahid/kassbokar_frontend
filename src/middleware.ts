14b2eca666717eb5394a3b01f687f6abca261238
the-jahid <aenor007@gmail.com>
2024-10-15 20:31:18 +0600

    'private_route_added'

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+export default authMiddleware({
+  publicRoutes: ['/', '/blogs', '/pricing'],
+})
 
 export const config = {
   matcher: [
@@ -9,6 +11,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
-
-
+}
